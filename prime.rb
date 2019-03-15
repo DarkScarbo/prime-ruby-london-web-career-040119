8370e3da602666b0490231101430692470a3ac4f
Xabier C <xabier_abia@hotmail.com>
2019-03-15 01:48:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -7,3 +7,17 @@ def prime?(num)
     end
   end
 end
+  if number == 0 or number == 1
+      return false
+
+  end
+    i = 2
+    limit = number / i
+    while i < limit
+        if number % i == 0
+            return false
+        end
+      i += 1
+      limit = number / i
+    end
+  return true
