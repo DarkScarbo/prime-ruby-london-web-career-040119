506f79697bac569a9425cc8dc45a378d3b4a2078
Xabier C <xabier_abia@hotmail.com>
2019-03-15 01:50:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
 def prime?(num)
   i = 2
-  limit = number / i
+  limit = num / i
     while i < limit
       if number % i == 0
         return false
       end
     i += 1
-    limit = number / i
+    limit = num / i
   end
 return true
 end
